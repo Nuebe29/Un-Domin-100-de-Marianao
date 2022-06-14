18428847394a3df393113af151a73f40c937edf4
JKY45 <juankyespinosa@gmail.com>
2022-06-14 15:04:52 -0400

    jky

---
+++ app/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Domino;
+mano<IFicha> player1 = new mano<IFicha>();
+mano<IFicha> player2 = new mano<IFicha>();
+mano<IFicha> player3 = new mano<IFicha>();
+mano<IFicha> player4 = new mano<IFicha>();
+mano<IFicha> player5 = new mano<IFicha>();
+int count = 0;
+
+for(int i = 0; i<7;i++){
+    for(int j = i; j< 7;j++){
+        
+        Ficha f = new Ficha(i,j);
+        count++;
+        if(count<8){player1.Add(f);continue;}
+        else if(count<15){player2.Add(f);continue;}
+        else if(count<22){player3.Add(f); continue;}
+        else{player4.Add(f);}
+        
+    }
+}
+player1.Sort();
+player2.Sort();
+player3.Sort();
+player4.Sort();
+
+List<IFicha> tablero = new List<IFicha>();
+
+mano<IFicha> [] juego = new mano<IFicha>[4];
+juego[0] = player1;
+juego[1] = player2;
+juego[2] = player3;
+juego[3] = player4;
+Ley ley = new Ley();
+var winner = player5;
+
+while(true){
+    List<IFicha> a = tablero;
+    for(int i = 0; i<4;i++){
+        tablero = juego[i].juega(tablero,ley);
+        if(juego[i].list.Count==0){
+            winner = juego[i];
+            break;
+        }
+        // if(a==tablero){
+        //     winner = player1;
+        //     for(int j = 1; j< 4;j++){
+        //         if(juego[j].puntos()<winner.puntos())winner = juego[j];
+        //     }
+        //     break;
+        // }
+
+    }
+    if(winner!=player5)break;
+    
+
+    
+}
+
+System.Console.WriteLine(winner);
+
+
+
+
+
---
+++ class/Class1.cs
@@ -0,0 +1,150 @@
+﻿namespace Domino
+{
+    public interface IGame<Tmano> where Tmano : Imano<IFicha>
+    {
+        public Tmano[] players { get; }
+    }
+    public interface IFicha
+    {
+        public (int, int) value { get; }
+        public int sum { get; }
+    }
+    public class Ficha : IFicha
+    {
+        int c;
+        int d;
+        public Ficha(int a, int b)
+        {
+            c = a;
+            d = b;
+        }
+
+        public (int, int) value => (c, d);
+
+        public int sum => c + d;
+    }
+
+    public interface IFilter<T>
+    {
+        bool Apply(IFicha ficha, List<IFicha> tablero);
+    }
+    public class Ley : IFilter<IFicha>
+    {
+        public Ley()
+        {
+        }
+
+        public bool Apply(IFicha ficha, List<IFicha> tablero)
+        {
+            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) { return true; }
+            else if (ficha.value.Item2 == tablero[0].value.Item1) { return true; }
+            ficha = vira(ficha);
+            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) { return true; }
+            else if (ficha.value.Item2 == tablero[0].value.Item1) { return true; }
+            return false;
+
+        }
+        public IFicha vira(IFicha ficha)
+        {
+            Ficha devolver = new Ficha(ficha.value.Item2, ficha.value.Item1);
+            return devolver;
+
+        }
+    }
+
+    public interface Imano<TFicha> where TFicha : IFicha
+    {
+        void Add(TFicha ficha);
+        void Remove(TFicha ficha);
+
+        void Sort();
+
+        int Total { get; }
+    }
+    public class mano<TFicha> : Imano<TFicha> where TFicha : IFicha
+    {
+        public List<IFicha> list;
+
+
+        public mano()
+        {
+            list = new List<IFicha>();
+
+        }
+
+        public int Total => total();
+
+        public void Add(TFicha ficha)
+        {
+            list.Add(ficha);
+        }
+
+        public void Remove(TFicha ficha)
+        {
+            list.Remove(ficha);
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    if (list[j].sum < list[j + 1].sum) Swap(j, j + 1);
+                }
+            }
+
+        }
+        private void Swap(int a, int b)
+        {
+            IFicha temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+        public int total()
+        {
+            int devolver = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                devolver += list[i].sum;
+            }
+            return devolver;
+        }
+        public List<IFicha> juega(List<IFicha> tablero, IFilter<IFicha> ley)
+        {
+            if (tablero.Count==0)
+            {
+                tablero.Add(list[0]);
+                list.Remove(list[0]);
+            }
+            else
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (ley.Apply(list[i], tablero))
+                    {
+                        IFicha ficha = list[i];
+                        if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) tablero.Add(ficha);
+                        else if (ficha.value.Item2 == tablero[0].value.Item1) tablero.Insert(0, ficha);
+                        ficha = new Ficha(ficha.value.Item2, ficha.value.Item1);
+                        if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) tablero.Add(ficha);
+                        else if (ficha.value.Item2 == tablero[0].value.Item1) tablero.Insert(0, ficha);
+                        list.Remove(list[i]);
+                        break;
+                    }
+                }
+            }
+            return tablero;
+        }
+        public int puntos()
+        {
+            int a = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                a += list[i].sum;
+            }
+            return a;
+        }
+    }
+}
+
