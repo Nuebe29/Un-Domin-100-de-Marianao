3d47d4df03c35e3a264b495bbcb2e38dd825057e
JKY45 <juankyespinosa@gmail.com>
2022-06-21 21:06:19 -0400

    probando

--- class/Class1.cs
+++ class/Class1.cs
@@ -1,164 +1,210 @@
-﻿namespace Domino
+﻿namespace Engine;
+
+public class FichaClásica
 {
-    public class DominoBoard<T>{
-        List<IFicha<T>> tablero;
-        T left;
-        T right;
-        public DominoBoard(){
-
-        }
-    }
-    public interface IGame<Tmano,T> where Tmano : Imano<IFicha<T>,T>
+    public FichaClásica(int cara1, int cara3)
     {
-        public Tmano[] players { get; }
-    }
-    public interface IFicha<T>
-    {
-        public (T, T) value { get; }
-        public int peso { get; }
-    }
-    public class Ficha : IFicha<int>
-    {
-        int c;
-        int d;
-        public Ficha(int a, int b)
-        {
-            c = a;
-            d = b;
-        }
-
-        public (int, int) value => (c, d);
-
-        public int peso => c + d;
+        Cara1 = cara1;
+        Cara2 = cara3;
     }
 
-    public interface IFilter<TFicha>
+    public int Cara1 { get; }
+
+    public int Cara2 { get; }
+
+    public int Peso => Cara1 + Cara2;
+
+    public override string ToString()
     {
-        bool Apply(TFicha ficha, List<TFicha> tablero);
-    }
-    public class Ley : IFilter<Ficha>
-    {
-        public Ley()
-        {
-        }
-
-        public bool Apply(Ficha ficha, List<Ficha> tablero)
-        {
-            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) { return true; }
-            else if (ficha.value.Item2 == tablero[0].value.Item1) { return true; }
-            ficha = vira(ficha);
-            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) { return true; }
-            else if (ficha.value.Item2 == tablero[0].value.Item1) { return true; }
-            return false;
-
-        }
-        public Ficha vira(Ficha ficha)
-        {
-            Ficha devolver = new Ficha(ficha.value.Item2, ficha.value.Item1);
-            return devolver;
-
-        }
-    }
-
-    public interface Imano<TFicha,T> where TFicha : IFicha<T>
-    {
-        void Add(TFicha ficha);
-        void Remove(TFicha ficha);
-
-        void Sort();
-
-        int Total { get; }
-    }
-    public class mano<TFicha,T> : Imano<TFicha,T> where TFicha : IFicha<T>
-    {
-        public List<TFicha> list;
-
-
-        public mano()
-        {
-            list = new List<TFicha>();
-
-        }
-
-        public int Total => total();
-
-        public void Add(TFicha ficha)
-        {
-            list.Add(ficha);
-        }
-
-        public void Remove(TFicha ficha)
-        {
-            list.Remove(ficha);
-        }
-
-        public void Sort()
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                for (int j = 0; j < list.Count - 1; j++)
-                {
-                    if (list[j].peso < list[j + 1].peso) Swap(j, j + 1);
-                }
-            }
-
-        }
-        private void Swap(int a, int b)
-        {
-            TFicha temp = list[a];
-            list[a] = list[b];
-            list[b] = temp;
-        }
-        public int total()
-        {
-            int devolver = 0;
-            for (int i = 0; i < list.Count; i++)
-            {
-                devolver += list[i].peso;
-            }
-            return devolver;
-        }
-        public List<IFicha<T>> juega(List<IFicha<T>> tablero, IFilter<IFicha<T>> ley)
-        {
-            if (tablero.Count == 0)
-            {
-                tablero.Add(list[0]);
-                list.Remove(list[0]);
-            }
-            else
-            {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    if (ley.Apply(list[i], tablero))
-                    {
-                        IFicha<T> ficha = list[i];
-                        if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2 || ficha.value.Item2 == tablero[0].value.Item1)
-                        {
-                            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) tablero.Add(ficha);
-                            else if (ficha.value.Item2 == tablero[0].value.Item1) tablero.Insert(0, ficha);
-                        }
-                        else
-                        {
-                            ficha = new Ficha(ficha.value.Item2, ficha.value.Item1);
-                            if (ficha.value.Item1 == tablero[tablero.Count - 1].value.Item2) tablero.Add(ficha);
-                            else if (ficha.value.Item2 == tablero[0].value.Item1) tablero.Insert(0, ficha);
-                        }
-                        list.Remove(list[i]);
-                        break;
-                    }
-                }
-            }
-            return tablero;
-        }
-        public int puntos()
-        {
-            int a = 0;
-            for (int i = 0; i < list.Count; i++)
-            {
-                a += list[i].peso;
-            }
-            return a;
-        }
+        return $"[{Cara1},{Cara2}]";
     }
 }
+
+public class TableroClásico
+{
+    public TableroClásico()
+    {
+        Contenido = new List<FichaClásica>();
+    }
+
+    public List<FichaClásica> Contenido { get; private set; }
+
+    public int Entrada1 { get; set; }
+
+    public int Entrada2 { get; set; }
+}
+
+public class Mano
+{
+    public Mano()
+    {
+        Contenido = new List<FichaClásica>();
+    }
+    public List<FichaClásica> Contenido { get; }
+
+    public int Peso
+    {
+        get
+        {
+            int peso = 0;
+
+            foreach (var ficha in Contenido)
+            {
+                peso += ficha.Peso;
+            }
+
+            return peso;
+        }
+    }
+
+    public void Add(FichaClásica ficha)
+    {
+        Contenido.Add(ficha);
+    }
+    public void Remuve(FichaClásica ficha)
+    {
+        Contenido.Remove(ficha);
+    }
+}
+
+public static class Leyes
+{
+    public static bool EsJugable(FichaClásica ficha, TableroClásico tablero)
+    {
+        return EsJugable(ficha, tablero.Entrada1) || EsJugable(ficha, tablero.Entrada2);
+    }
+
+    public static bool EsJugable(FichaClásica ficha, int entrada)
+    {
+        return ficha.Cara1 == entrada || ficha.Cara2 == entrada;
+    }
+
+    public static bool EsJugable(Mano mano, TableroClásico tablero)
+    {
+        foreach (var ficha in mano.Contenido)
+        {
+            if (EsJugable(ficha, tablero)) return true;
+        }
+
+        return false;
+    }
+}
+
+public class JugadorRandom
+{
+    public JugadorRandom(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; }
+
+    public FichaClásica Jugada(List<FichaClásica> mano, TableroClásico tablero)
+    {
+        List<FichaClásica> fichasJugables = new List<FichaClásica>();
+
+        foreach (var ficha in mano)
+        {
+            if (Leyes.EsJugable(ficha, tablero)) fichasJugables.Add(ficha);
+        }
+
+        Random random = new Random();
+        return fichasJugables[random.Next(fichasJugables.Count)];
+    }
+
+    public FichaClásica Salida(List<FichaClásica> mano)
+    {
+        Random random = new Random();
+        int n = random.Next(mano.Count);
+        return mano[n];
+    }
+}
+
+public static class JuegoClásico
+{
+    public static void Colocar(FichaClásica ficha, TableroClásico tablero)
+    {
+        if (tablero.Contenido.Count == 0)
+        {
+            tablero.Contenido.Add(ficha);
+            tablero.Entrada1 = ficha.Cara1;
+            tablero.Entrada2 = ficha.Cara2;
+        }
+
+        else if (Leyes.EsJugable(ficha, tablero.Entrada1))
+        {
+            tablero.Contenido.Add(ficha);
+            tablero.Entrada1 = ficha.Cara1 == tablero.Entrada1 ? ficha.Cara2 : ficha.Cara1;
+        }
+
+        else if (Leyes.EsJugable(ficha, tablero.Entrada2))
+        {
+            tablero.Contenido.Add(ficha);
+            tablero.Entrada2 = ficha.Cara1 == tablero.Entrada2 ? ficha.Cara2 : ficha.Cara1;
+        }
+
+        else throw new Exception("Esa Jugada no es posible");
+    }
+
+    public static JugadorRandom DecidirGanador(List<JugadorRandom> jugadores, List<Mano> manos)
+    {
+        JugadorRandom ganador = new JugadorRandom("nadie");
+        int min = int.MaxValue;
+
+        for (int i = 0; i < jugadores.Count; i++)
+        {
+            if (manos[i].Peso < min)
+            {
+                ganador = jugadores[i];
+                min = manos[i].Peso;
+            }
+        }
+
+        return ganador;
+    }
+}
+
+public static class Generador
+{
+    public static List<FichaClásica> Genera(int n)
+    {
+        List<FichaClásica> fichas = new List<FichaClásica>();
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = i; j <= n; j++)
+            {
+                fichas.Add(new FichaClásica(i,j));
+            }
+        }
+
+        return fichas;
+    }
+}
+
+public static class Repartidor
+{
+    public static void Reparte(List<FichaClásica> mazo, int c, List<Mano> manos)
+    {
+        if (mazo.Count < c * manos.Count)
+        {
+            throw new Exception("No es posible repartir");
+        }
+
+        else
+        {
+            Random r = new Random();
+
+            foreach (var mano in manos)
+            {
+                for (int i = 0; i < c; i++)
+                {
+                    int n = r.Next(mazo.Count);
+                    mano.Contenido.Add(mazo[n]);
+                    mazo.Remove(mazo[n]);
+                }
+            }
+        }
+    }
+} 
+
 
