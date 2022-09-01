b17c3012014478dd5e098e84a1b4d8cbbe9eba74
JKY45 <juankyespinosa@gmail.com>
2022-09-01 01:48:02 -0400

    antuaaaaaan

--- Solution/Engine/Partida.cs
+++ Solution/Engine/Partida.cs
@@ -1,17 +1,19 @@
 namespace Engine;
 
-public class Partida<T>{
-    public Tablero<T> Tablero {get; }
-    public List<Player<T>> Players { get; set;}    
+public class Partida<T>
+{
+    public Tablero<T> Tablero { get; }
+    public List<Player<T>> Players { get; set; }
     private Referee<T> Referee { get; }
 
-    public Partida(IEndcondition<T> endcondition, IWincondition<T> wincondition, IMatcher<T> matcher,IDealer<T> dealer,
-     IGenerador<T> generador,List<Player<T>> players, T n, ITurner<T> turner, int cantidad)
+    public Partida(IEndcondition<T> endcondition, IWincondition<T> wincondition, IMatcher<T> matcher, IDealer<T> dealer,
+     IGenerador<T> generador, List<Player<T>> players, T n, ITurner<T> turner, int cantidad)
     {
         Tablero = new Tablero<T>();
         Tablero.Hoja.Salida = true;
-        Players = players;        
-        Referee = new Referee<T>(endcondition,wincondition,matcher,dealer,generador,players,n,Tablero, turner,cantidad);
+        Players = players;
+        Referee = new Referee<T>(endcondition, wincondition, matcher, dealer, generador, players, n, Tablero, turner, cantidad);
     }
     public void Run() => Referee.Run();
+    public bool Run(int a)=> Referee.Run(a);
 }
