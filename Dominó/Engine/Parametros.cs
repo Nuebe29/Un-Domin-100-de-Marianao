0b915813fa9210040114a0655cc88f0dbab6b8aa
Nuebe29 <anthuanmontesdeoca@gmail.com>
2022-09-07 10:07:27 -0400

    Una bella interfaz grafica que no funciona :´(

---
+++ Dominó/Engine/Parametros.cs
@@ -0,0 +1,20 @@
+﻿namespace Engine;
+
+public class Parametros
+{
+    public Parametros()
+    {
+        Matcher = new MatcherClasico();
+        WinCondición = new GanadorCálsico();
+        Endcondition = new EndconditionClasico();
+        Turner = new TurnerClasico();
+        Players = new List<Player<int>>();
+    }
+    
+    public IMatcher<int> Matcher { get; set; }
+    public IWincondition<int> WinCondición { get; set; }
+    public IEndcondition<int> Endcondition { get; set; }
+    public ITurner<int> Turner { get; set; }
+    public List<Player<int>> Players { get; set; }
+    
+}
